91dc7a49eeb2a6f9983423acced8dd52ecc22da0
MaxGOUET <maxime.gouet1@gmail.com>
2026-04-02 14:57:10 +0200

    ajout bcrypt + jwt + mongoose et connect à la DB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
-const postRoutes = require("./routes/postRoutes");
-const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+
+// const postRoutes = require("./routes/postRoutes");
+// const userRoutes = require("./routes/userRoutes");
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const app = express();
 
